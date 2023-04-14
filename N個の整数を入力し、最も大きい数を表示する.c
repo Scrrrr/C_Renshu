c46c336ce4853737b7be65513e0f29807e596c1f
V1 <vividboooy@protonmail.com>
2023-04-14 18:10:08 +0900

    N個の整数を入力し、最も大きい数を表示する.c

---
+++ N個の整数を入力し、最も大きい数を表示する.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define NUMBER 8
+
+int main(void) {
+	char str[NUMBER + 1][32];
+	int a[NUMBER];
+	int max;
+	int i, j;
+
+
+	for (i = 0; i < (NUMBER - 1); i++) {
+		printf("number%d:", i+1);	
+		fgets(str[i], 32, stdin);		
+		a[i] = atoi(str[i]);
+	}
+
+	max = a[0];
+
+	for (j = 0; j < (NUMBER - 1); j++) {
+		if (max < a[j])
+			max = a[j];
+	}
+				
+	printf("max = %d\n", max);
+		
+	return 0;
+ }
