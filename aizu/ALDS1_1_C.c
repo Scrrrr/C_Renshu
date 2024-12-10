3e171caedfe991c6da977e40f3c691e6f9c038ec
Scrrrr <Scrrrr@users.noreply.github.com>
2024-12-10 23:57:10 +0900

    メモ追記

--- aizu/ALDS1_1_C.c
+++ aizu/ALDS1_1_C.c
@@ -1,5 +1,19 @@
 #include<stdio.h>
 #include<stdlib.h>
+/*
+配列変数A [2,3,5...]
+
+if(A[i] % x == 0)
+goto:素数じゃない
+
+配列変数Aを制約の数の半分くらい確保して配列を１で初期化する
+
+グローバル変数iで配列Aの要素数を共有
+
+素数である：
+    A[i] = 素数;
+    i++;
+*/
 
 int prime(int n, int array[])
 {
