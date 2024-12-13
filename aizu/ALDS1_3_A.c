3134e92e5ea59c30bfbde5975bf13f228f90313f
Scrrrr <Scrrrr@users.noreply.github.com>
2024-12-13 18:46:02 +0900

    margin

--- aizu/ALDS1_3_A.c
+++ aizu/ALDS1_3_A.c
@@ -1,44 +1,107 @@
 #include<stdio.h>
-#define Stack_Size 100
+#include<stdlib.h>
+#include<string.h>
 
-int push(int *Stack, int *sp, int n)
+#define STACK_SIZE 100
+
+typedef enum{
+    IT_NULL,  //NULLの時
+    IT_DIGIT, //数値の時
+    IT_ADD,
+    IT_SUB,
+}ItemType;
+
+typedef struct{
+    ItemType type;
+    int value;
+}Item;
+
+Item ItemNull;
+Item stack[STACK_SIZE];
+int stack_sp;
+
+Item parse_arg(const char *s)
 {
-    if(*sp > Stack_Size)
+    if(!strcmp(s,"+"))
     {
-        return -1;
+        return (Item) { .type=IT_ADD};
+    }else if(!strcmp(s,"-")){
+        return (Item) { .type=IT_SUB};
+    }else{
+        return (Item) { .type=IT_DIGIT, .value=atoi(s)};
     }
-    Stack[*sp] = n;
-    (*sp)++;
-    return 0;
+}
+
+Item stack_push(Item item)
+{
+    if(stack_sp >= STACK_SIZE)
+    {
+        fprintf(stderr, "stack overflow!\n");
+        exit(EXIT_FAILURE);
+    }
+
+    stack[stack_sp++] = item;
+}
+
+int stack_pop(Item *item)
+{
+    if(stack_sp <= 0)
+    {
+        return 0;
+    }
+
+    *item = stack[--stack_sp];
+}
+
+Item calc(void)
+{
+    Item item;
+    if(!stack_pop(&item))
+    {
+        return ItemNull;
+    }
+
+    switch (item.type)
+    {
+    default: return item; break;
+    case IT_ADD:
+    {
+        Item op2 = calc();
+        Item op1 = calc();
+        Item result;
+        result.type = IT_DIGIT;
+        result.value = op1.value + op2.value;
+        stack_push(result);
+        return calc();
+    }break;
+    case IT_SUB:
+    {
+        Item op2 = calc();
+        Item op1 = calc();
+        Item result;
+        result.type = IT_DIGIT;
+        result.value = op1.value - op2.value;
+        stack_push(result);
+        return calc();
+    }
+    
+    }
+
+    return ItemNull;
 }
 
 int main(void)
 {
-    int c,n;
-    int spp = 0;
-    int *sp = &spp;
-    int Stack[Stack_Size];
-
-    do
+    char strings[200];
+    fgets(strings,sizeof(strings),stdin);
+    for(int i = 0; i != '\n'; i++)
     {
-        printf("Select Menu:");
-        c = getchar();
-        if(c == 'i' || c == 'I')//push
-        {
-            printf("data -->");
-            scanf("%d",&n);
-            if(push(Stack,sp,n) == -1)
-            {
-                printf("error");
-            }
-        }
-        if(c == 's' || c == 'S')//show stack
-        {
-            for(int i = 0; i < sizeof(Stack) / sizeof(Stack[0]); i++)
-            {
-                printf("%d ",Stack[i]);
-            }
-            putchar('\n');
-        }
-    }while(c != EOF);
+        Item item = parse_arg(&strings[i]);
+        stack_push(item);
+    }
+
+    Item result = calc();
+
+    printf("%d",result.value);
+    fflush(stdout);
 }
